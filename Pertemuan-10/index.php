078b58573e3751d087abeff9e2afa4308f0c123d
ahmaduunnail <ahmadunnail0@gmail.com>
2024-04-11 19:21:58 +0700

    💳 Pertemuan 10 - Praktikum 3

--- Pertemuan-10/index.php
+++ Pertemuan-10/index.php
@@ -27,7 +27,9 @@
                 echo "<tr>
                             <td>" . $no++ . "</td><td>" . $row['nama'] . "</td>
                             <td>" . $kelamin . "</td><td>" . $row['alamat'] . "</td>
-                            <td>" . $row['no_telp'] . "</td><td><a href='#' onclick='konfirmasiHapus(" . $row['id'] . ", \"" . $row['nama'] . "\")'>Hapus</a></td>
+                            <td>" . $row['no_telp'] . "</td>
+                            <td><a href='edit.php?id=". $row['id'] ."'>Edit</a> | 
+                            <a href='#' onclick='konfirmasiHapus(" . $row['id'] . ", \"" . $row['nama'] . "\")'>Hapus</a></td>
                             </tr>";
             }
             echo "</table>";
